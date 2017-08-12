61a724abb75fae2a0daeb975cf2958f07eb3de8e
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2017-08-12 16:26:02 -0300

    Adicionada tabela para visualização dos usuários cadastrados

--- user.php
+++ user.php
@@ -31,30 +31,8 @@ function page_content(){
 	
 	validate_error();
 	validate_success();
-	
-	// Este trecho chama o cadastro de contato
-	// Incluindo classe para criação de tag
-	include_once 'model/class/Tag.class.php';
 
-	// Criando botão para chamar o modal
-	$a = new Tag('a');
-	$a->class = "btn btn-info";
-	$a->href="#add_contact";
-	
-		// Arranjo
-		$atribute = "data-toggle";
-	$a->$atribute = "modal";
-	$a->add_content("Novo contato");
-	$a->show_tag();
-	
-	// Modal que conterá o form de cadastro
-	$modal_id = "add_contact";
-	$modal_title = "Adicionar Contato";
-	$modal_file = "view/forms/add_contact.html";
-	$modal_text = "";
-	
-	include_once 'view/modal.html';
-	
+	include_once 'view/table.html';
 }
 
 
